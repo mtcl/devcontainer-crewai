8b594b5ee19d8f1f0d2ba450ae2337be925a7199
Mukul Tripathi <mukul.nifft@gmail.com>
2024-05-05 19:06:16 -0500

    fixed double v-env name issue

--- .devcontainer/set_python_env.sh
+++ .devcontainer/set_python_env.sh
@@ -4,8 +4,5 @@ PYTHON_ENV=$1
 
 python3 -m venv /opt/$PYTHON_ENV  \
         && export PATH=/opt/$PYTHON_ENV/bin:$PATH \
-        && echo "source /opt/$PYTHON_ENV/bin/activate" >> ~/.bashrc
 
-source /opt/$PYTHON_ENV/bin/activate
-
-pip3 install -r ./requirements/requirements.txt
+/opt/$PYTHON_ENV/bin/pip3 install -r ./requirements/requirements.txt
